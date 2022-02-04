f028059e2a6c7930aacfb5f8ffa2b8001e7a1de9
Edwin Gathura <egathura@gmail.com>
2022-02-04 18:36:31 -0500

    initial commit

---
+++ src/test/java/net/hopkins22/demoapi/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hopkins22.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
