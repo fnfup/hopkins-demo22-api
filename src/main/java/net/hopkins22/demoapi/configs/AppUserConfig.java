769eca3b05e1128dc8ed079f20f83df9d5aadd35
Edwin Gathura <egathura@gmail.com>
2022-02-06 18:52:31 -0500

    update commandlinerunner prioritization to use order annotation

--- src/main/java/net/hopkins22/demoapi/configs/AppUserConfig.java
+++ src/main/java/net/hopkins22/demoapi/configs/AppUserConfig.java
@@ -5,6 +5,7 @@ import net.hopkins22.demoapi.repository.IAppUserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import java.util.List;
 
@@ -12,13 +13,15 @@ import java.util.List;
 public class AppUserConfig {
 
     @Bean("appuser_data")
+    @Order(10)
     CommandLineRunner commandLineRunner(IAppUserRepository repo) {
         return args -> {
-
+            System.out.println("----s App Users s-----");
             AppUser testUser = new AppUser("hopkinsdemouser");
             AppUser tom = new AppUser("tombrady");
 
             repo.saveAll(List.of(tom, testUser));
+            System.out.println("----e App Users e-----");
         };
     }
 
