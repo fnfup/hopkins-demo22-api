61dce44363c67eed1484fdfb9e09b9d55cd04966
Edwin Gathura <egathura@gmail.com>
2022-02-06 19:58:01 -0500

    move user order init to separate thread;

--- src/main/java/net/hopkins22/demoapi/configs/UserOrderConfig.java
+++ src/main/java/net/hopkins22/demoapi/configs/UserOrderConfig.java
@@ -11,15 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 @Configuration
 @Order(Ordered.LOWEST_PRECEDENCE)
@@ -42,63 +35,13 @@ public class UserOrderConfig {
 
             System.out.println("----s User Orders s-----");
 
-            Optional<AppUser> targetUser = appUserRepo
-                    .findByUsername("hopkinsdemouser")
-                    .stream().findFirst();
+            Thread userOrdersBootInit = new UserOrdersBoostrapper(
+                    aggregateRoot, appUserRepo, catalogRepo, userMusicRepo);
 
-            if (targetUser.isPresent()) {
-                List<MusicTrack> orderItems = getRandomOrderList(4, targetUser.get());
-
-//                for (MusicTrack m: orderItems) {
-//                    System.out.println(m);
-//                }
-                System.out.println(
-                    orderItems.stream().reduce("",
-                            (acc, curr) -> acc + "," +
-                                    String.valueOf(curr.getId()), String::concat));
-
-                aggregateRoot.commitUserOrder(targetUser.get(), orderItems);
-            }
+            userOrdersBootInit.start();
             System.out.println("----e User Orders e-----");
 
         };
     }
-
-    private List<MusicTrack> getRandomOrderList(Integer count, AppUser user) {
-
-        List<UserMusic> currentLibrary = userMusicRepo.findAllByUserId(user.getId());
-
-        System.out.println("Current user library count: " + currentLibrary.size());
-
-        System.out.println(
-                currentLibrary.stream().reduce("",
-                        (acc, curr) -> acc + "," +
-                                String.valueOf(curr.getTrack().getId()), String::concat));
-
-        List<MusicTrack> allMusic = catalogRepo.findAll();
-
-        List<MusicTrack> filteredMusic = allMusic.stream().filter(track -> {
-            return !currentLibrary.stream().anyMatch(libraryTrack -> {
-                return track.getId() == libraryTrack.getTrack().getId();
-            });
-        }).collect(Collectors.toList());
-
-        Random rand = new Random();
-
-        List<MusicTrack> orderList = new ArrayList<>();
-
-        for(int i = 0; i < count; i++) {
-            int randomIndex = rand.nextInt(filteredMusic.size());
-
-            MusicTrack track = filteredMusic.get(randomIndex);
-            orderList.add(track);
-
-            // remove so we don't have duplicates
-            filteredMusic.remove(randomIndex);
-        }
-
-        return orderList;
-    }
-
 
 }
