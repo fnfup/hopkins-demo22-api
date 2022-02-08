7c5f6af155feab1e86e3584ab076a4f999585fa0
Edwin Gathura <egathura@gmail.com>
2022-02-08 03:48:54 -0500

    still attempting to force cors

--- src/main/java/net/hopkins22/demoapi/configs/DemoWebSecurityConfig.java
+++ src/main/java/net/hopkins22/demoapi/configs/DemoWebSecurityConfig.java
@@ -1,13 +1,18 @@
 package net.hopkins22.demoapi.configs;
 
 import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-import javax.annotation.Resource;
-import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 
 //@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -35,27 +40,43 @@ public class DemoWebSecurityConfig extends AADWebSecurityConfigurerAdapter {
             "pathvar:{*oauth*}"
     };
 
-    @Autowired // <- for autowired solution
-    RestCorsConfigSource corsConfigSource;
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*",
+                "https://hopkins-demo22-api-sc-hopkins-demo22-ui.azuremicroservices.io"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        System.out.println("---!!! Setting up CORS config !!!---");
+        return bean;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         System.out.println("Setting up web security");
         super.configure(http);
         http
-//                .requiresChannel()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/catalog**", "/user**", "/order**"
+                ).authenticated()
+                .antMatchers(AUTH_WHITELIST).permitAll();
+
+
+        //                .requiresChannel()
 //                .anyRequest().requiresInsecure()
 //                .antMatchers("/login/oauth2/code/").requiresInsecure()
 //                .anyRequest().requiresSecure()
 //                .and()
-                .cors().configurationSource(corsConfigSource);
-
-        http
-                .authorizeRequests()
-                .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers(
-                        "/catalog**", "/user**", "/order**"
-                ).authenticated();
     }
 
 
